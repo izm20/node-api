36ff9812c82d2f8529511735fd875145fa1136fa
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-11 17:31:48 +0000

    FIX entrypoints order

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const movieRouter = require("./src/api/movies");
 
 app.use(express.json());
 
+app.use("/movies", movieRouter);
+
 app.get("/", (req, res) => {
   res.json({name: "api-node"})
 });
-
-app.use("/movies", movieRouter);
 
 app.listen(3000, () => {
   console.log("Listen on port 3000");
